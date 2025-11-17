2d74c0302fbd030d856a99524021a79c9262f7b1
SaraPettinari <sara.pettinari@unicam.it>
2025-11-17 21:40:22 +0100

    [init] BPIC17 analysis

--- datasets/bpic17/main.py
+++ datasets/bpic17/main.py
@@ -4,9 +4,9 @@ from ocean_lib import pipeline, AggrSpecification, AggrStep
 @pipeline(first_load=False)
 def build_aggr_spec(log, ekg):
     aggr_basic = [
-        AggrStep(aggr_type="ENTITIES", ent_type="Invoice", group_by=[ekg.type_tag], where='value >= 5000', attr_aggrs=[]),
-        AggrStep(aggr_type="ENTITIES", ent_type="Invoice", group_by=[ekg.type_tag], where='value < 5000', attr_aggrs=[]),
-        AggrStep(aggr_type="EVENTS", ent_type= None, group_by=[log.event_activity], where=None, attr_aggrs=[])]
+        AggrStep(aggr_type="ENTITIES", ent_type="Application", group_by=[ekg.type_tag], where='type = "New credit"', attr_aggrs=[]),
+        AggrStep(aggr_type="ENTITIES", ent_type="Application", group_by=[ekg.type_tag], where='type <> "New credit"', attr_aggrs=[]),
+        AggrStep(aggr_type="EVENTS", ent_type= None, group_by=[log.event_activity,'lifecycle'], where=None, attr_aggrs=[])]
     
     return AggrSpecification(aggr_basic)
 
