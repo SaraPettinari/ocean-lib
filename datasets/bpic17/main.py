6619d4d0f26154785cad7f7fcf02d3613c394508
SaraPettinari <sara.pettinari@unicam.it>
2025-12-15 15:30:24 +0100

    [refactor] adopted evaluation steps

--- datasets/bpic17/main.py
+++ datasets/bpic17/main.py
@@ -1,11 +1,16 @@
 from ocean_lib import pipeline, AggrSpecification, AggrStep
 
+#https://link.springer.com/article/10.1007/s13740-021-00122-1/figures/25
 
 @pipeline(first_load=False)
 def build_aggr_spec(log, ekg):
     aggr_basic = [
-        AggrStep(aggr_type="ENTITIES", ent_type="Application", group_by=[ekg.type_tag], where='type = "New credit"', attr_aggrs=[]),
-        AggrStep(aggr_type="ENTITIES", ent_type="Application", group_by=[ekg.type_tag], where='type <> "New credit"', attr_aggrs=[]),
+        #AggrStep(aggr_type="ENTITIES", ent_type="Application", group_by=[ekg.type_tag], where='type = "New credit"', attr_aggrs=[]),
+        #AggrStep(aggr_type="ENTITIES", ent_type="Application", group_by=[ekg.type_tag], where='type <> "New credit"', attr_aggrs=[]),
+        AggrStep(aggr_type="ENTITIES", ent_type="Application", group_by=[ekg.type_tag], where=None, attr_aggrs=[]),
+        AggrStep(aggr_type="ENTITIES", ent_type="Offer", group_by=[ekg.type_tag], where=None, attr_aggrs=[]),
+        AggrStep(aggr_type="ENTITIES", ent_type="Workflow", group_by=[ekg.type_tag], where=None, attr_aggrs=[]),
+        AggrStep(aggr_type="ENTITIES", ent_type="Resource", group_by=[ekg.type_tag], where=None, attr_aggrs=[]),
         AggrStep(aggr_type="EVENTS", ent_type= None, group_by=[log.event_activity,'lifecycle'], where=None, attr_aggrs=[])]
     
     return AggrSpecification(aggr_basic)
--- datasets/soccer/main.py
+++ datasets/soccer/main.py
@@ -2,36 +2,13 @@ from ocean_lib import pipeline, AggrSpecification, AggrStep, AttrAggr, Aggregati
 
 @pipeline(first_load=False)
 def build_aggr_spec(log, ekg):
-    # example 
-    aggr_example = [AggrStep(aggr_type="ENTITIES", ent_type= "teamId", group_by=["country"], where=None, attr_aggrs=[AttrAggr(name='city', function=AggregationFunction.MULTISET)]),
-                    AggrStep(aggr_type="ENTITIES", ent_type= "playerId", group_by=["role"], where='birthYear > 1990', attr_aggrs=[]),
-                    AggrStep(aggr_type="EVENTS", ent_type= None, where=None, group_by=[log.event_activity, "teamId","playerId"], 
-                                    attr_aggrs=[AttrAggr(name=log.event_timestamp, function=AggregationFunction.MINMAX),
-                                                AttrAggr(name="matchId", function=AggregationFunction.MULTISET)])]
+    
     # running example:
     aggr_running = [AggrStep(aggr_type="ENTITIES", ent_type= "playerId", group_by=["role"], where=None, attr_aggrs=[]),
-                    AggrStep(aggr_type="EVENTS", ent_type= None, where=None, group_by=[log.event_activity, "teamId","playerId"], 
+                    AggrStep(aggr_type="ENTITIES", ent_type= "teamId", group_by=["wyId"], where=None, attr_aggrs=[]),
+                    AggrStep(aggr_type="EVENTS", ent_type= None, where=None, group_by=[log.event_activity], 
                                     attr_aggrs=[AttrAggr(name=log.event_timestamp, function=AggregationFunction.MINMAX),
                                                 AttrAggr(name="matchId", function=AggregationFunction.MULTISET)])]
-    
-    # 4-step:
-    aggr_example_2 =[
-    AggrStep(aggr_type="ENTITIES", ent_type= "playerId", group_by=["role"], where='birthYear > 1990', attr_aggrs=[]),
-    AggrStep(aggr_type="ENTITIES", ent_type= "pos_orig", group_by=["zone"], where=None, attr_aggrs=[]),
-    AggrStep(aggr_type="EVENTS", ent_type= None, where='matchId = "2575959"', group_by=[log.event_activity, "teamId","playerId"], 
-                     attr_aggrs=[AttrAggr(name=log.event_timestamp, function=AggregationFunction.MINMAX),
-                                 AttrAggr(name="teamId", function=AggregationFunction.MULTISET)]),
-    AggrStep(aggr_type="EVENTS", ent_type= None, where='matchId <> "2575959"', group_by=[log.event_activity, "teamId","playerId"], 
-                     attr_aggrs=[AttrAggr(name=log.event_timestamp, function=AggregationFunction.MINMAX),
-                                 AttrAggr(name="teamId", function=AggregationFunction.MULTISET)]) ]
-    
-    aggr_basic = [AggrStep(aggr_type="ENTITIES", ent_type= "playerId", group_by=["role"], where=None, attr_aggrs=[]),
-                    AggrStep(aggr_type="EVENTS", ent_type= None, where=None, group_by=[log.event_activity, "playerId"], 
-                                    attr_aggrs=[])]
-    
-    aggr_basic0 = [
-        AggrStep(aggr_type="ENTITIES", ent_type= "playerId", group_by=["role"], where=None, attr_aggrs=[]),
-        AggrStep(aggr_type="EVENTS", ent_type= None, group_by=[log.event_activity, "teamId","playerId"], where='matchId = "2575959"', attr_aggrs=[AttrAggr(name=log.event_timestamp, function=AggregationFunction.MINMAX)])]
     
     return AggrSpecification(aggr_running)
 
