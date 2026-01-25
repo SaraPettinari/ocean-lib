2c506a0ade2c56951d9d02a0bd16e56fc21d40bc
SaraPettinari <sara.pettinari@unicam.it>
2026-01-25 16:26:52 +0100

    agg steps updated

--- datasets/order-management/main.py
+++ datasets/order-management/main.py
@@ -1,14 +1,20 @@
 from ocean_lib import pipeline, AggrSpecification, AggrStep, AttrAggr, AggregationFunction  
 
-@pipeline(first_load=False)
-def build_aggr_spec(log, ekg):
-    
-    step1 = AggrStep(aggr_type="ENTITIES", ent_type= "customers", group_by=["id"], where=None, attr_aggrs=[AttrAggr(name='id', function=AggregationFunction.MULTISET)])
-    step2 = AggrStep(aggr_type="ENTITIES", ent_type= "orders", group_by=[ekg.type_tag], where='price >= 1000', attr_aggrs=[AttrAggr(name='price', function=AggregationFunction.AVG)])
-    step3 = AggrStep(aggr_type="ENTITIES", ent_type= "employees", group_by=['role'], where=None, attr_aggrs=None)
-    step4 = AggrStep(aggr_type="EVENTS", ent_type= None, where=f'{log.event_timestamp} >= datetime("2024-03-31T00:00:00")', group_by=[log.event_activity, "customers", "orders"], attr_aggrs=[]) # I want to check all the orders after Easter
-    step5 = AggrStep(aggr_type="EVENTS", ent_type= None, where=f'{log.event_timestamp} < datetime("2024-03-31T00:00:00")', group_by=[log.event_activity, "customers", "orders"], attr_aggrs=[]) # I want to check all the orders before Easter
 
-    return AggrSpecification(steps=[step1, step2, step3,step4,step5])
+@pipeline(first_load=True)
+def build_aggr_spec(log, ekg):
+    '''
+    This aggregation defines separate classes for high-value and low-value orders (i.e., >= 1000 and < 1000)
+    and aggregates events accordingly to support customer-centric process analysis and order-value centric insights.
+    '''
+    steps = [
+    AggrStep(aggr_type="ENTITIES", ent_type= "customers", group_by=["id"], where=None, attr_aggrs=[]),
+    AggrStep(aggr_type="ENTITIES", ent_type= "orders", group_by=[ekg.type_tag], where='price >= 1000', attr_aggrs=[AttrAggr(name='price', function=AggregationFunction.AVG)]),
+    AggrStep(aggr_type="ENTITIES", ent_type= "orders", group_by=[ekg.type_tag], where='price < 1000', attr_aggrs=[AttrAggr(name='price', function=AggregationFunction.AVG)]),
+    AggrStep(aggr_type="EVENTS", ent_type= None, group_by=[log.event_activity], where=None, attr_aggrs=[]),
+    # AggrStep(aggr_type="ENTITIES", ent_type= "employees", group_by=[ekg.type_tag], where=None, attr_aggrs=[]),
+    # AggrStep(aggr_type="ENTITIES", ent_type= "items", group_by=[ekg.type_tag], where=None, attr_aggrs=[]),
+    ]
+    return AggrSpecification(steps)
 
 
--- datasets/soccer/main.py
+++ datasets/soccer/main.py
@@ -2,13 +2,41 @@ from ocean_lib import pipeline, AggrSpecification, AggrStep, AttrAggr, Aggregati
 
 @pipeline(first_load=False)
 def build_aggr_spec(log, ekg):
-    
-    # running example:
+
+    # baseline example:
+    '''
+    This aggregation aims to reproduce a baseline analysis by reconstructing an OC-DFG.
+    '''
+    baseline = [AggrStep(aggr_type="ENTITIES", ent_type= "playerId", group_by=[ekg.type_tag], where=None, attr_aggrs=[]),
+                AggrStep(aggr_type="ENTITIES", ent_type= "teamId", group_by=[ekg.type_tag], where=None, attr_aggrs=[]),
+                AggrStep(aggr_type="EVENTS", ent_type= None, where=None, group_by=[log.event_activity], 
+                                            attr_aggrs=None)]
+        
+    # running example (matchId = 2575959):
+    '''
+    This aggregation aims to analyze player and team activities during a specific soccer match, to analyze teams strategies.
+    '''
     aggr_running = [AggrStep(aggr_type="ENTITIES", ent_type= "playerId", group_by=["role"], where=None, attr_aggrs=[]),
                     AggrStep(aggr_type="ENTITIES", ent_type= "teamId", group_by=["wyId"], where=None, attr_aggrs=[]),
                     AggrStep(aggr_type="EVENTS", ent_type= None, where=None, group_by=[log.event_activity], 
                                     attr_aggrs=[AttrAggr(name=log.event_timestamp, function=AggregationFunction.MINMAX),
                                                 AttrAggr(name="matchId", function=AggregationFunction.MULTISET)])]
     
+    # full dataset example:
+    # we want to aggregate by teamId and position 
+    # so that we can see all events for each team and position 
+    # and anlyze which areas are more active in terms of events
+    '''
+    This aggregation aims to provide a comprehensive overview of team events positions across all matches,
+    facilitating the analysis of event distributions for each team (i.e., anlyze which areas are more active in terms of events).
+    '''
+    aggr_full = [
+        AggrStep(aggr_type="ENTITIES", ent_type="teamId", group_by=["wyId"], where=None, attr_aggrs=[]),
+        AggrStep(aggr_type="ENTITIES", ent_type="pos_orig", group_by=["wyId"], where=None, attr_aggrs=[]),
+        AggrStep(aggr_type="EVENTS", ent_type=None, group_by=[log.event_activity], where=None, 
+                 attr_aggrs=[AttrAggr(name=log.event_timestamp, function=AggregationFunction.MINMAX),
+                             AttrAggr(name="playerId", function=AggregationFunction.MULTISET)])
+    ]
+                
     return AggrSpecification(aggr_running)
 
